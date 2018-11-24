b5a6f4a467eb82362b5726aa9a60d3ea3728b300
igorxxl8 <you@example.com>
2018-11-24 15:21:06 +0300

    add new queries

--- core/db_queries.py
+++ core/db_queries.py
@@ -6,13 +6,32 @@ def get_all_goods():
     return cursor.fetchall()
 
 
+def get_data_by_query(query):
+    pass
+
+
+def get_good():
+    cursor.execute("""SELECT * FROM Goods""")
+    return cursor.fetchall()
+
+
 def get_all_shops():
     cursor.execute("""SELECT * FROM Shops""")
     return cursor.fetchall()
 
 
+def get_shop(shop_id):
+    cursor.execute("""SELECT * FROM Shops WHERE Shops.shop_id = {}""".format(shop_id))
+    return cursor.fetchall()
+
+
 def get_all_warehouse():
     cursor.execute("""SELECT * FROM Warehouses""")
+    return cursor.fetchall()
+
+
+def get_warehouse(warehouse_id):
+    cursor.execute("""SELECT * FROM Warehouses WHERE Warehouses.warehouse_id = {}""".format(warehouse_id))
     return cursor.fetchall()
 
 
@@ -25,5 +44,4 @@ def get_all_warehouse_contains():
     cursor.execute("SELECT * FROM WarehouseContains")
     return cursor.fetchall()
 
-
-#conn.commit()
+# conn.commit()
